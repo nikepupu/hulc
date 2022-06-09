1d12577e04c2f214d205ebab3095e367cd721a2e
Steven Gong <nikepupu9@gmail.com>
2022-06-09 13:17:29 -0700

    modify path

--- load_arnold.sh
+++ load_arnold.sh
@@ -1,1 +1,1 @@
-python hulc/load_arnold.py trainer.gpus=-1 datamodule.root_data_dir=/media/nikepupu/fast/All_frames/pickup_object/ datamodule/datasets=arnold
+python hulc/load_arnold.py trainer.gpus=-1 datamodule.root_data_dir=/media/nikepupu/fast/frame12update/pickup_object datamodule/datasets=arnold
